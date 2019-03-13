b1a6d77ac89ce96444b8928bca96ae59ddf939c8
Gaspar <gasparcaamal12@gmail.com>
2019-03-12 18:08:04 -0600

    primer cambio
    Operadores que utilizamos

---
+++ Operadores.py
@@ -0,0 +1,13 @@
+operaciones = 1-5*6+20 # para ver las operaciones 
+operación2 = 4
+
+operacion = "2"
+operacion2 = "2" # estamos haciedo una concadenación esdecir uniendo dos textos, ya que utilizamos las comillas y tomo los valores como cadenas de texto
+
+resultado = operacion+operacion2
+print(resultado)
+
+'''
+las tres comillas sirven para hacer comentarios
+en blocke
+'''
