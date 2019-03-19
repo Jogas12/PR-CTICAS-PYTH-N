6b8aa248a0ab549bc7711274507f80374d27f9b4
Gaspar <gasparcaamal12@gmail.com>
2019-03-19 16:35:18 -0600

    Entrada de datos personales
    Utilizamos input

---
+++ Entradapersona.py
@@ -0,0 +1,12 @@
+'''veremos  entrada de datos dentro de variables, para esto se utilizará 
+el input'''
+
+nombre = input("ingresar nombre: ") #la entrada tenemos que guardarla dentro de una variable
+
+apellido = input("ingresar apellido: ") # para comentar lineas de código es ctrl+corchete derch
+
+edad = input("ingresar edad: ")
+
+imprimimos lo capturado por teclado # input es para pedir al usuario que ingrese un dato
+
+ print(f"Bienvenido {nombre} {apellido}, su edad es : {edad}") # la f nos sirve para poder llamar a las variables que tenemos dentro d elos corchetes y nos hace una cadena d etexto libre
