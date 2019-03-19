9ffd84750e830c6cd7dfedaceee416ec78d59bcf
Gaspar <gasparcaamal12@gmail.com>
2019-03-19 16:42:37 -0600

    Entrada de números
    Utilizamos int

---
+++ Entradanumeros.py
@@ -0,0 +1,6 @@
+número1= int(input("ingresar un numero: ")) #int nos sirve para válidar números enteros, ya que sino lo tomaria como texto al número
+número2= int(input("ingresar un numero: ")) #El int al principio nos dice que el número que vamos a ingresar va ser un núnero y no un texto
+resultado = número1 + número2
+
+
+print (f"El resultado es: {resultado}")
